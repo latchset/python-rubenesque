17c1a1fd585fcbc51bf38938b15d85606057b25e
Nathaniel McCallum <npmccallum@redhat.com>
2015-10-26 16:32:21 -0400

    Move codecs.eddsa => codecs.cfrg
    Additionally, update the encoding to comply with the latest discussions
    on the CFRG list. Specifically:
    http://www.ietf.org/mail-archive/web/cfrg/current/msg07256.html
    
    This implies that all points can now be encoded in this format.

--- rubenesque/codecs/cfrg.py
+++ rubenesque/codecs/cfrg.py
@@ -24,47 +24,64 @@
 # OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
 # OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
+"""
+Implements codec according to the protocol discussed on CFRG:
+http://www.ietf.org/mail-archive/web/cfrg/current/msg07256.html
+"""
+
 
 def encode(point):
     """
     >>> from ..curves.sec import secp224r1, secp521r1
-    >>> from ..curves.cfrg import edwards25519
+    >>> from ..curves.cfrg import edwards25519, edwards448
 
     >>> encode(secp224r1.generator())
-    Traceback (most recent call last):
-    AssertionError: assert (224 % 8) > 0
+    b'!\\x1d\\\\\\x11\\xd6\\x8024"\\x11\\xc2V\\xd3\\xc1\\x03J\\xb9\\x90\\x132\\x7f\\xbf\\xb4k\\xbd\\x0c\\x0e\\xb7\\x00'
 
     >>> encode(secp521r1.generator())
     b"f\\xbd\\xe5\\xc21~~\\xf9\\x9bBj\\x85\\xc1\\xb3H3\\xde\\xa8\\xff\\xa2'\\xc1\\x1d\\xfe(Y\\xe7\\xefw^K\\xa1\\xba=Mk`\\xaf(\\xf8!\\xb5?\\x059\\x81d\\x9cB\\xb4\\x95#f\\xcb>\\x9e\\xcd\\xe9\\x04\\x04\\xb7\\x06\\x8e\\x85\\xc6\\x00"
 
     >>> encode(edwards25519.generator())
     b'Xfffffffffffffffffffffffffffffff'
-    """
-    assert point.bits() % 8 > 0
 
-    i = point.primary | ((point.secondary & 1) << point.bits())
-    return i.to_bytes((point.bits() + 7) // 8, 'little')
+    >>> encode(edwards448.generator())
+    b'\\xfe\\xff\\xff\\xff\\xff\\xff\\xff\\xff\\xff\\xff\\xff\\xff\\xff\\xff\\xff\\xff\\xff\\xff\\xff\\xff\\xff\\xff\\xff\\xff\\xff\\xff\\xff\\x7f\\xff\\xff\\xff\\xff\\xff\\xff\\xff\\xff\\xff\\xff\\xff\\xff\\xff\\xff\\xff\\xff\\xff\\xff\\xff\\xff\\xff\\xff\\xff\\xff\\xff\\xff\\xff\\x7f\\x00'
+    """
+
+    l = point.bits() // 8 + 1
+    b = (point.secondary & 1) << (l * 8 - 1)
+    return (point.primary | b).to_bytes(l, 'little')
 
 
 def decode(cls, bytes):
     """
     >>> from ..curves.sec import secp224r1, secp521r1
-    >>> from ..curves.cfrg import edwards25519
+    >>> from ..curves.cfrg import edwards25519, edwards448
 
-    >>> decode(secp521r1, encode(secp521r1.generator()))
-    secp521r1(00C6858E06B70404E9CD9E3ECB662395B4429C648139053FB521F828AF606B4D3DBAA14B5E77EFE75928FE1DC127A2FFA8DE3348B3C1856A429BF97E7E31C2E5BD66, 011839296A789A3BC0045C8A5FB42C7D1BD998F54449579B446817AFBD17273E662C97EE72995EF42640C550B9013FAD0761353C7086A272C24088BE94769FD16650)
+    >>> g5 = secp224r1.generator() * 5
+    >>> decode(secp224r1, encode(g5)) == g5
+    True
 
-    >>> decode(edwards25519, encode(edwards25519.generator()))
-    edwards25519(6666666666666666666666666666666666666666666666666666666666666658, 216936D3CD6E53FEC0A4E231FDD6DC5C692CC7609525A7B2C9562D608F25D51A)
+    >>> g5 = secp521r1.generator() * 5
+    >>> decode(secp521r1, encode(g5)) == g5
+    True
+
+    >>> g5 = edwards25519.generator() * 5
+    >>> decode(edwards25519, encode(g5)) == g5
+    True
+
+    >>> g5 = edwards448.generator() * 5
+    >>> decode(edwards448, encode(g5)) == g5
+    True
     """
-    assert len(bytes) == (cls.bits() + 7) // 8
-    assert cls.bits() % 8 > 0
 
-    mask = 1 << cls.bits()
-    primary = int.from_bytes(bytes, 'little')
-    bit = primary & mask >> cls.bits()
-    primary &= ~mask
+    l = cls.bits() // 8 + 1
+    assert len(bytes) == l
 
-    point = cls.recover(primary, bit)
+    p = int.from_bytes(bytes, 'little')
+    b = (p >> (l * 8 - 1)) & 1
+    p &= ~(1 << (l * 8 - 1))
+
+    point = cls.recover(p, b)
     assert point.is_valid
     return point
